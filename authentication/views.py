56378e6f30c78c17ec0b83136f8d2daa7c96c62f
amj-web <andrewmjarrett@gmail.com>
2023-01-08 02:50:54 +0500

    Register View has been created

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from authentication.serializer import RegisterSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+# This will responsible for registering the users
+class RegisterAPIView(GenericAPIView):
+    # no need for authentication for registration
+    authentication_classes=[] 
+    # we will use our RegisterSerializer for parsing
+    serializer_class=RegisterSerializer
+    
+    def post(self,request):
+        # send all data to our serializer
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid(): #if everything is valid than save
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
