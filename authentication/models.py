22aab76b3d1ce48f6af7d1b6410fdf4f235fb6d1
amj-web <andrewmjarrett@gmail.com>
2023-01-08 02:39:07 +0500

    models for user auth is created and registered in admin

--- authentication/models.py
+++ authentication/models.py
@@ -5,7 +5,12 @@ from helper.models import TrackingModels
 from django.contrib.auth.models import PermissionsMixin,AbstractBaseUser,UserManager
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+import jwt
+from django.conf import settings
+from datetime import datetime,timedelta
 
+# Create and save a user with the given username, email, and password.
 class MyUserManager(UserManager):
     def _create_user(self, username, email, password, **extra_fields):
         """
@@ -40,3 +45,45 @@ class MyUserManager(UserManager):
             raise ValueError("Superuser must have is_superuser=True.")
 
         return self._create_user(username, email, password, **extra_fields)
+
+class User(AbstractBaseUser,PermissionsMixin,TrackingModels):
+    # validators for basic validations
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+    email = models.EmailField(_("email address"), blank=False,unique=True)
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+
+    objects = MyUserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+    # generate JWT token for authentication
+    @property
+    def token(self):
+        token=jwt.encode({'username':self.username,'email':self.email,'exp':datetime.utcnow()+timedelta(hours=24)},settings.SECRET_KEY,algorithm='HS256')
+        return token
