8838e0fb283570ebaa4005fe241082d90544afc3
amj-web <andrewmjarrett@gmail.com>
2023-01-08 02:45:00 +0500

    serializer for authentication is completed

--- authentication/serializer.py
+++ authentication/serializer.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from authentication.models import User
 
+# converting objects into data types understandable by ReactJs
+
+# Register serializer willhandle data regarding user registrations 
 class RegisterSerializer(serializers.ModelSerializer):
     password=serializers.CharField(max_length=125,min_length=6,write_only=True)
     class Meta:
@@ -8,3 +11,16 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields=("username","email","password","id")
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+# Login serializer willhandle data regarding user login 
+class LoginSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(max_length=125,min_length=6,write_only=True)
+    class Meta:
+        model=User
+        fields=("username","email","password","token","id")
+        read_only_fields=['token']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=("username","email","id")
