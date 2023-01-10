f825e8e552328ec34624f5447085ad2956e6ee1d
amj-web <andrewmjarrett@gmail.com>
2023-01-10 18:47:31 +0500

    category model created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from helper.models import TrackingModels
+from authentication.models import User
 
-# Create your models here.
+class Category(TrackingModels):
+    name =models.CharField(default='Learning',max_length=150,unique=True,null=False)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+         return self.name
