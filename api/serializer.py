c71ec17d69b257f634f06b18e0b64bc3bd82c8a2
amj-web <andrewmjarrett@gmail.com>
2023-01-10 19:03:37 +0500

    serializers of todo and category is completed

---
+++ api/serializer.py
@@ -0,0 +1,17 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from api.models import ToDo,Category
+from authentication.models import User
+
+class CategorySerializer(ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    class Meta:
+        model=Category
+        fields=('id','name','user')
+class TodoSerializer(ModelSerializer):
+    image = serializers.CharField(read_only=True)
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    class Meta:
+        model=ToDo
+        fields=('id','title','description','dueDate','image','status','priority','user','category')
